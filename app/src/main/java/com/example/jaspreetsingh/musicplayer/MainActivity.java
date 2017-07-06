9e1e3289beb7e5d8d50a2031afae290d8fb99f0e
Jaspreet Singh (DELL) <cgc.jsbaidwan@gmail.com>
2017-07-06 16:34:09 +0530

    using Async callbacks add toast when the song is complete

--- app/src/main/java/com/example/jaspreetsingh/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/musicplayer/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick (View view){
         mediaPlayer.start();
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                Toast.makeText(MainActivity.this, "I am done", Toast.LENGTH_LONG).show();
+            }
+        });
     }
     });
     // Set onclickListener for the pause button
