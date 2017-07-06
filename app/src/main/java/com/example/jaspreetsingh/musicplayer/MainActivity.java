6d32d3e901d7506e16dd952302c1811afddf6b9d
Jaspreet Singh (DELL) <cgc.jsbaidwan@gmail.com>
2017-07-06 11:02:19 +0530

    minor adjustments, working music player with two button: play and pause

--- app/src/main/java/com/example/jaspreetsingh/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/musicplayer/MainActivity.java
@@ -9,24 +9,23 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Button to play the music
-    public Button play;
-    // Button to pause the music
-    public Button pause;
+
     // MediaPlayer
-    public MediaPlayer mediaPlayer = new MediaPlayer();
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Find the button that play the media file
-        play = (Button) findViewById(R.id.play_button);
-        // Find the button that play the media file
-        pause = (Button) findViewById(R.id.pause_button);
+        mediaPlayer = MediaPlayer.create(this, R.raw.song);
 
-    mediaPlayer = MediaPlayer.create(this, R.raw.song);
+        // Find the button that play the media file
+        Button play = (Button) findViewById(R.id.play_button);
+        // Find the button that play the media file
+         Button pause = (Button) findViewById(R.id.pause_button);
+
+
 
     // Set OnclickListener to the play button
     play.setOnClickListener(new View.OnClickListener()
